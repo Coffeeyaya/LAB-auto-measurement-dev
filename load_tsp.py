f7531f2ddbf4193586fe6677869ce7d184455fa4
Coffeeyaya <coffee20120704@gmail.com>
2026-02-11 12:55:52 +0800

    test

--- load_tsp.py
+++ load_tsp.py
@@ -4,26 +4,12 @@ rm = pyvisa.ResourceManager()
 RESOURCE = "GPIB0::26::INSTR"
 inst = rm.open_resource(RESOURCE)
 
-inst.timeout = 10000
-inst.write_termination = '\n'
-inst.read_termination = '\n'
-
-print(inst.query("print(localnode.model)"))
 inst.write("script.delete('get_sweep')")
-inst.write("reset()")
+
 with open("get_sweep.tsp", "r") as f:
-    tsp_code = f.read()
+    for line in f:
+        inst.write(line.rstrip())
 
-inst.write(tsp_code)
-print(inst.query("print(script.catalog())"))
+print("Upload complete.")
 inst.write("get_sweep.run()")
-data = []
-
-while True:
-    try:
-        line = inst.read()
-        data.append(float(line))
-        print("Measured I:", line)
-    except:
-        break
-inst.write("smua.source.output = smua.OUTPUT_OFF")
+inst.write("smua.source.output = suma.OUTPUT_OFF")
