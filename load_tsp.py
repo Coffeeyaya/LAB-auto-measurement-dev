3514dd66224fd578f89ddc5748fb9a5f2d99ef2c
Coffeeyaya <coffee20120704@gmail.com>
2026-02-11 13:02:09 +0800

    test

--- load_tsp.py
+++ load_tsp.py
@@ -1,15 +1,30 @@
 import pyvisa
-
 rm = pyvisa.ResourceManager()
+
 RESOURCE = "GPIB0::26::INSTR"
 inst = rm.open_resource(RESOURCE)
 
-inst.write("script.delete('get_sweep')")
+inst.write_termination = '\n'
+inst.read_termination = '\n'
 
-with open("get_sweep.tsp", "r") as f:
+# Delete old script if exists
+inst.write("script.delete('gate_sweep')")
+
+# Upload new script line by line
+with open("gate_sweep.tsp", "r") as f:
     for line in f:
-        inst.write(line.rstrip())
+        if line.strip():  # skip empty lines
+            inst.write(line.rstrip())
 
 print("Upload complete.")
-inst.write("get_sweep.run()")
-inst.write("smua.source.output = suma.OUTPUT_OFF")
+
+# Run the sweep
+inst.write("run()")  # call the global function
+
+# Read all output lines from print()
+while True:
+    try:
+        line = inst.read()
+        print("Measured current:", line)
+    except pyvisa.errors.VisaIOError:
+        break
