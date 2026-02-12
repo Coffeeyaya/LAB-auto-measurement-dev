8145c898fb2ec77dbe5c884221298635d64ab48d
Coffeeyaya <coffee20120704@gmail.com>
2026-02-12 17:04:56 +0800

    test

--- keithley/run.bat
+++ keithley/run.bat
@@ -1,10 +1,13 @@
 @echo off
 
-REM Start measurement script
-start "Measurement" cmd /k python keithley.py
+
+REM Start live plot script
+start "Live Plot" cmd /k python plot.py
 
 REM Small delay to ensure file is created
 timeout /t 2 >nul
 
-REM Start live plot script
-start "Live Plot" cmd /k python plot.py
+REM Start measurement script
+start "Measurement" cmd /k python keithley.py
+
+
