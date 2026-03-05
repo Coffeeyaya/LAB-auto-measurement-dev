e73e9c3ca0d8d9a000de740901d295104ab1ff61
Coffeeyaya <coffee20120704@gmail.com>
2026-03-05 13:20:45 +0800

    test

--- power_bs.py
+++ power_bs.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     try:
         pm.zero_sensor() # zero power meter
         for i, wavelength in enumerate(wavelength_arr):
-            pp, power = find_pp_for_target_power(conn=conn, pm=pm, channel=channel_arr[i], target_power=100, wavelength=wavelength,
+            pp, power = find_pp_for_target_power(conn=conn, pm=pm, channel=int(channel_arr[i]), target_power=100, wavelength=wavelength,
                                     pp_min = 5, pp_max = 120)
             pp_table[i] = pp
             power_table[i] = power
