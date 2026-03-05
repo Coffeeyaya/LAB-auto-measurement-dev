56ff4e14b191068555813894c4b20f96d9a87c2e
Coffeeyaya <coffee20120704@gmail.com>
2026-03-05 16:31:41 +0800

    test

--- time_dependent.py
+++ time_dependent.py
@@ -193,8 +193,10 @@ if __name__ == "__main__":
     device_number = ''
     run = ''
     FILENAME = f"time_{device_number}_{run}.csv" #
-    
-    duration = 10 #
+    deplete_time = 5
+    duration = 5
+    on_time = 30
+    off_time = 10
     Vg_on = 0 #
     Vg_off = -2 #
     channel = 6
@@ -204,16 +206,16 @@ if __name__ == "__main__":
         (0.0,  0, duration),   #
         (Vg_off,  0, duration),#
         (Vg_on,  0, duration), #
-        (Vg_on,  1, duration), # light on
-        (Vg_on,  0, duration), # light off
-        (Vg_off, 0, duration), #
+        (Vg_on,  1, on_time), # light on
+        (Vg_on,  0, off_time), # light off
+        (Vg_off, 0, deplete_time), #
           #
     ]
     for i in range(5):
         my_sequence.extend([(Vg_on,  0, duration), #
-            (Vg_on,  1, duration),
-            (Vg_on,  0, duration),
-            (Vg_off, 0, duration)])
+            (Vg_on,  1, on_time),
+            (Vg_on,  0, off_time),
+            (Vg_off, 0, deplete_time)])
     my_sequence.append((0, 0, duration))
     
     run_measurement(RESOURCE_ID, LIGHT_IP, FILENAME, channel=channel, sequence=my_sequence, Vd_target=1)
