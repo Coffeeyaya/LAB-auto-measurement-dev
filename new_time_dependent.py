330af3d6bfd9a3cdb61d0fd6fcd7a6edc622201e
Coffeeyaya <coffee20120704@gmail.com>
2026-03-09 12:26:13 +0800

    test

--- new_time_dependent.py
+++ new_time_dependent.py
@@ -115,7 +115,7 @@ class TimeDepWorker(QThread):
 
                 # Generate the sequence block dynamically from this config's parameters
                 sequence = []
-                wavelength_arr = np.array(params.get("wavelength_arr", [450, 532, 660])).astype(str)
+                wavelength_arr = np.array(params.get("wavelength_arr", [450, 532, 660])).astype(int).astype(str)
                 channel_arr = np.array(params.get("channel_arr", [0, 3, 6])).astype(int).astype(str)
                 
                 table = pd.read_csv(Path("calibration") / "single_power_multi_wavelength.csv")
