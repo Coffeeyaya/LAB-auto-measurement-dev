eff7031962121ecd05ee7a5c8974363677d84db8
Coffeeyaya <coffee20120704@gmail.com>
2026-06-08 18:28:04 +0800

    test

--- kCube.py
+++ kCube.py
@@ -24,6 +24,13 @@ class WaveplateController:
         # Operates in raw hardware steps to ensure accuracy
         self.motor = Thorlabs.KinesisMotor(self.serial_number)
         self.steps_per_degree = 1919.641
+        
+        # Set velocity parameters: (min_vel, acceleration, max_vel)
+        # Converting user-specified deg/s and deg/s^2 to raw steps
+        max_vel_steps = int(25 * self.steps_per_degree)
+        accel_steps = int(10 * self.steps_per_degree)
+        self.motor.set_velocity_parameters(0, accel_steps, max_vel_steps)
+        
         self.is_homed = False
 
     def home(self, wait=True):
